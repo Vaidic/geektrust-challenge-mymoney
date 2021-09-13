e7bc1fb225e36867774e564825ab578bf8b10b71
Vaidic Joshi <vaidic@vaidicjoshi.in>
2021-09-13 13:16:52 +0530

    Initialize Gradle (Spring Boot) Project

---
+++ src/main/java/in/vaidicjoshi/geektrust/backend/mymoney/MyMoneyApplication.java
@@ -0,0 +1,13 @@
+package in.vaidicjoshi.geektrust.backend.mymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/vaidicjoshi/geektrust/backend/mymoney/MyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vaidicjoshi.geektrust.backend.mymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
