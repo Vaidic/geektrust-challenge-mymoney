97208e7aeaca6698c1db52d263d7fe1eaa606384
Vaidic Joshi <vaidic@vaidicjoshi.in>
2021-09-13 18:12:05 +0530

    Added Logging, checking for shell/batch mode, reformatted code

--- src/main/java/in/vaidicjoshi/geektrust/backend/mymoney/MyMoneyApplication.java
+++ src/main/java/in/vaidicjoshi/geektrust/backend/mymoney/MyMoneyApplication.java
@@ -1,13 +1,43 @@
 package in.vaidicjoshi.geektrust.backend.mymoney;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
+import java.util.InputMismatchException;
+
+/**
+ * @author Vaidic Joshi
+ * @date 12/09/21
+ */
 @SpringBootApplication
-public class MyMoneyApplication {
+@Order(-1)
+@Slf4j
+public class MyMoneyApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyMoneyApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MyMoneyApplication.class, args);
+  }
 
+  @Override
+  public void run(String... args) throws Exception {
+    if (args.length < 1) {
+      log.error("No input arguments were supplied");
+      throw new InputMismatchException(
+          "Please specify input file, or to run in CLI mode provide SHELL as argument");
+    } else if (args.length != 1) {
+      log.error("No input arguments were supplied");
+      throw new InputMismatchException(
+          "Please specify only the input file, or to run in CLI mode provide SHELL as argument");
+    }
+    String input = args[0];
+    if ("shell".equalsIgnoreCase(input)) {
+      log.info("Switching to SHELL Mode");
+      return;
+    }
+    log.info("Switching to BATCH-PROCESSING Mode");
+    // todo: process file - executeCommandsFromFile(input);
+  }
 }
