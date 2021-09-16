ccc5ac927f2647294232dc41f7020977a1c688b8
Vaidic Joshi <vaidic@vaidicjoshi.in>
2021-09-16 21:39:35 +0530

    Update Utils class methods

--- src/main/java/in/vaidicjoshi/geektrust/backend/mymoney/service/MyMoneyDriver.java
+++ src/main/java/in/vaidicjoshi/geektrust/backend/mymoney/service/MyMoneyDriver.java
@@ -2,7 +2,6 @@ package in.vaidicjoshi.geektrust.backend.mymoney.service;
 
 import com.google.common.base.Strings;
 import in.vaidicjoshi.geektrust.backend.mymoney.enums.SupportedCommand;
-import in.vaidicjoshi.geektrust.backend.mymoney.utils.MyMoneyUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +12,7 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.DataFormatException;
@@ -30,6 +30,13 @@ public class MyMoneyDriver {
     this.myMoneyService = myMoneyService;
   }
 
+  /**
+   * Reads the input from a file lin-by-line and executes each line as a command.
+   *
+   * @param filename
+   * @return
+   * @throws IOException
+   */
   public List<String> executeCommandsFromFile(String filename) throws IOException {
     try (Stream<String> lines = Files.lines(Paths.get(filename))) {
       List<String> outputs =
@@ -37,7 +44,7 @@ public class MyMoneyDriver {
               .filter(l -> !Strings.isNullOrEmpty(l))
               .map(this::processLineAsCommand)
               .collect(Collectors.toList());
-      MyMoneyUtils.display(outputs);
+      display(outputs);
       return outputs;
 
     } catch (IOException e) {
@@ -46,6 +53,13 @@ public class MyMoneyDriver {
     }
   }
 
+  /**
+   * Maps a string containing command and input parameters to correct function call and executes the
+   * command.
+   *
+   * @param line
+   * @return
+   */
   public String processLineAsCommand(String line) {
     String output = null;
     int supportedAssetClass = myMoneyService.getSupportedAssetClass();
@@ -92,6 +106,14 @@ public class MyMoneyDriver {
     return output;
   }
 
+  /**
+   * Extract double values from string containing space seperated list of values.
+   *
+   * @param skip
+   * @param limit
+   * @param commandAndInputs
+   * @return
+   */
   private List<Double> getDoubles(int skip, int limit, String[] commandAndInputs) {
     return Arrays.stream(commandAndInputs)
         .skip(skip)
@@ -100,10 +122,25 @@ public class MyMoneyDriver {
         .collect(Collectors.toList());
   }
 
+  /**
+   * Validates the inputs parameters received to have correct cardinality.
+   *
+   * @param commandAndInputs
+   * @param size
+   */
   private void validateInputSize(String[] commandAndInputs, int size) {
     if (commandAndInputs.length != size + 1) {
       throw new InputMismatchException(
           "Please check the command " + String.join(" ", commandAndInputs));
     }
+  }
+
+  /**
+   * Prints to console.
+   *
+   * @param outputs
+   */
+  private static void display(List<String> outputs) {
+    outputs.stream().filter(Objects::nonNull).forEach(System.out::println);
   }
 }
